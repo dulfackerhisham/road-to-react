5a327882e511ef35884e5c1a4237d60815f65aca
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-06-26 17:20:58 +0530

    react counter project

---
+++ 01counter/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+
+  const [counter, setCounter] = useState(4)
+  //let counter = 5
+
+  const addValue = () => {
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+    setCounter(counter - 1)
+    setCounter(counter - 1)
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1>React Counter Project </h1>
+      <h3>Counter Value : {counter}</h3>
+      <button onClick={addValue}>Add Value</button> {" "}
+      <button onClick={removeValue} >Remove Value</button>
+    </>
+  )
+}
+
+export default App
