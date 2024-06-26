29af958c07f1ef3b0fb8ce7879391334e52c056f
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-06-26 20:39:00 +0530

    configured tailwind

---
+++ 02tailwindProps/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl bg-orange-600 p-4 rounded-lg' >Hello World with tailwind</h1>
+    </>
+  )
+}
+
+export default App
