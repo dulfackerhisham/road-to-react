d77892470a9948f7e1e7b4c92a59f5a7ecc8b56f
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-06-26 15:26:05 +0530

    playing with jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,13 +2,16 @@ import Whatsapp from "./Whatsapp"
 
 const title = <h1>Vite app title</h1>
 
+
+const greet = "good morning"
 function App() {
 
   return (
     <>
     {title}
     <title></title>
-    <h3> vite App Component</h3>
+    <h3> vite App Component {5 + 5}</h3>
+    {greet}
     <Whatsapp />
     {Whatsapp()}
     </>
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,6 +1,20 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: "https//google.com",
+//         target: '_blank',
+//     },
+//     children: 'Click me to visit Google'
+// }
+
+const Hlo = () => (<h1>hlo arrow function</h1>);
+
+const reactElement = <a href="http//google.com" target='_blank'> Visit Google</a>
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
 )
